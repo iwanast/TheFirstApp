77031cedd71341682af980b8f62b208b6c8e5e5a
Iwana Städeli <78117193+iwanast@users.noreply.github.com>
2021-12-16 22:00:38 +0100

    fixed loading not more than 120

--- scripts/script.js
+++ scripts/script.js
@@ -3,82 +3,15 @@ let ageButton = document.getElementById("change-age-button");
 let wrapperPrograms = document.getElementById("posts-summaries");
 let wrapperEpisodes = document.getElementById("episodes-summaries")
 let listenButtons = document.getElementsByClassName("listen-button");
+let bodyEpisodes = document.getElementById("body-episodes");
+let bodyPrograms = document.getElementById("body-programs");
+let moreEpisodes = document.getElementById("more-episodes")
 let auditivSignal;
 
 // Start to get data from the API
 async function startRequestAPI(url) {
   let result = await makeRequest("GET", url);
   return result; 
-}
-
-//Loading the programes for age 3-8 when starting the page
-//populatePostsWithPrograms("http://api.sr.se/api/v2/programs/index?programcategoryid=2&pagination=false&format=json")
-
-// Change the programes for the range 3-8 or 9-13 on display and the buttoncolor and -text
-if(ageButton){
-ageButton.onclick = function(event){
-  if (parseInt(event.target.getAttribute("data-lowestAge")) === 3){
-    wrapperPrograms.innerHTML = "";
-    populatePostsWithPrograms("http://api.sr.se/api/v2/programs/index?programcategoryid=132&pagination=false&format=json")
-    ageButton.innerHTML = "Display the programes for age 3-8"; 
-    ageButton.setAttribute("data-lowestAge", "9");
-    ageButton.style.background = "#bf10e2";
-  }else{
-    wrapperPrograms.innerHTML = "";
-    populatePostsWithPrograms("http://api.sr.se/api/v2/programs/index?programcategoryid=2&pagination=false&format=json")
-    ageButton.innerHTML = "Display the programes for age 9-13"; 
-    ageButton.setAttribute("data-lowestAge", "3")
-    ageButton.style.background = "#1b9bbb";
-  }
-}
-}
-
-
-async function populatePostsWithPrograms(url){
-  let result = await startRequestAPI(url)
-  result = JSON.parse(result);
-  for(let i = 0; i < result.programs.length; i++){
-    createPostProgram(result.programs[i]);
-  }
-}
-
-// When the page loads all the episodes-posts will be created 
-if(document.getElementById("body-single-post")){
-  populatePostsWithEpisodes();
-}
-
-// Depending if there are episodes and if pods or broadcasts, the different functions are called to show them on the page
-async function populatePostsWithEpisodes(){
-  let id = JSON.parse(findQuery("id"));
-  let url = `http://api.sr.se/api/v2/episodes/index?programid=${id}&audioquality=hi&pagination=false&format=json`;
-  let result = await startRequestAPI(url)
-  result = JSON.parse(result);
-  if(!result.episodes[0]){
-    alert("There are no episodes")
-  }else if(result.episodes[0].listenpodfile){
-    setMainTitle("Pods for this program", "maintitle-episodes");
-    for(let i = result.episodes.length - 1; i >= 0; i--){
-      if(result.episodes[i].listenpodfile){
-        createPodEpisodes(result.episodes[i]);
-      } 
-    }
-  }else if(result.episodes[0].broadcast){
-    setMainTitle("Broadcasts for this program from the last month", "maintitle-episodes")
-    for(let i = result.episodes.length - 1; i >= 0; i--){
-      if(result.episodes[i].broadcast){
-        createBroadcastEpisodes(result.episodes[i]);
-      }
-    }
-  }else{
-    alert("There are no broadcasts or pods for this program")
-    return
-  }
-  setListenButtons();
-}
-
-function findQuery(param) {
-  var urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get(param);
 }
 
 // Getting the data from the API
@@ -106,6 +39,49 @@ function makeRequest(method, url) {
   });
 }
 
+function findQuery(param) {
+  var urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+function setMainTitle(title, htmlId){
+  document.getElementById(htmlId).innerHTML = title; 
+}
+
+//////////////////////////////// PROGRAM PAGE //////////////////////////////////
+
+//Loading the programes for age 3-8 when starting the program-page
+if(bodyPrograms){
+populatePostsWithPrograms("http://api.sr.se/api/v2/programs/index?programcategoryid=2&pagination=false&format=json")
+}
+
+// Change the programes for the range 3-8 or 9-13 on display and the buttoncolor and -text
+if(ageButton) {
+  ageButton.onclick = function(event) {
+    if(parseInt(event.target.getAttribute("data-lowestAge")) === 3){
+      wrapperPrograms.innerHTML = "";
+      populatePostsWithPrograms("http://api.sr.se/api/v2/programs/index?programcategoryid=132&pagination=false&format=json")
+      ageButton.innerHTML = "Display the programes for age 3-8"; 
+      ageButton.setAttribute("data-lowestAge", "9");
+      ageButton.style.background = "#bf10e2";
+    }else {
+      wrapperPrograms.innerHTML = "";
+      populatePostsWithPrograms("http://api.sr.se/api/v2/programs/index?programcategoryid=2&pagination=false&format=json")
+      ageButton.innerHTML = "Display the programes for age 9-13"; 
+      ageButton.setAttribute("data-lowestAge", "3")
+      ageButton.style.background = "#1b9bbb";
+    }
+  }
+}
+
+async function populatePostsWithPrograms(url){
+  let result = await startRequestAPI(url)
+  result = JSON.parse(result);
+  for(let i = 0; i < result.programs.length; i++){
+    createPostProgram(result.programs[i]);
+  }
+}
+
 function createPostProgram(programData) {
   if(!programData) return null; 
   if(programData.hasondemand || programData.haspod)
@@ -122,9 +98,65 @@ function createPostProgram(programData) {
    </li>`;
 }
 
+///////////////////////// EPISODE PAGE///////////////////////
+
+// When the episode-page loads all the episodes-posts will be created 
+if(bodyEpisodes){
+  populatePostsWithEpisodes();
+}
+
+// Depending if there are episodes and if pods or broadcasts, the different functions are called to show episodes on the page
+async function populatePostsWithEpisodes(){
+  let id = JSON.parse(findQuery("id"));
+  let url = `http://api.sr.se/api/v2/episodes/index?programid=${id}&audioquality=hi&pagination=false&format=json`;
+  let result = await startRequestAPI(url)
+  result = JSON.parse(result);
+  if(!result.episodes[0]) {
+    alert("There are no episodes")
+  }else if(result.episodes[0].listenpodfile) {
+    setMainTitle("Pods for this program", "maintitle-episodes");
+    if(result.episodes.length > 120){
+      for(let i = 0; i < 120; i++){
+        createPodEpisodes(result.episodes[i]);
+      }
+      moreEpisodes.innerHTML = "There are more then 120 episodes but only 120 showing here!";
+    } else{
+      for(let i = result.episodes.length - 1; i >= 0; i--){
+        if(result.episodes[i].listenpodfile){
+          createPodEpisodes(result.episodes[i]);
+        } 
+      }
+      moreEpisodes.innerHTML = "";
+    }
+  }else if(result.episodes[0].broadcast) {
+    setMainTitle("Broadcasts for this program from the last month", "maintitle-episodes")
+    if(result.episodes.length > 120){
+      for(let i = 0; i < 120; i++){
+        if(result.episodes[i].broadcast){
+          createBroadcastEpisodes(result.episodes[i]);
+        }
+      }
+      moreEpisodes.innerHTML = "There are more then 120 episodes but only 120 showing here!";
+    }else {
+      for(let i = result.episodes.length - 1; i >= 0; i--){
+        if(result.episodes[i].broadcast){
+          createBroadcastEpisodes(result.episodes[i]);
+        }
+      }
+      moreEpisodes.innerHTML = "";
+    }
+    
+    
+  }else {
+    alert("There are no broadcasts or pods for this program")
+    return
+  }
+  setListenButtons();
+}
+
 function createPodEpisodes(podEpisodeData){
-if(!podEpisodeData) return null; 
-    let audioType = declareAudioTypeFromFileEnding(podEpisodeData.listenpodfile.url);
+  if(!podEpisodeData) return null; 
+  let audioType = declareAudioTypeFromFileEnding(podEpisodeData.listenpodfile.url);
   wrapperEpisodes.innerHTML += `<li class="single-post-wrapper__post">
     <audio id="myAudio${podEpisodeData.id}">
       <source src="${podEpisodeData.listenpodfile.url}" type="audio/${audioType}">
@@ -145,9 +177,8 @@ if(!podEpisodeData) return null;
 
 function createBroadcastEpisodes(ondemandEpisodData){
   if(!ondemandEpisodData) return null; 
-  console.log(ondemandEpisodData.broadcast.broadcastfiles[0].url)
   let audioType = declareAudioTypeFromFileEnding(ondemandEpisodData.broadcast.broadcastfiles[0].url);
-wrapperEpisodes.innerHTML += `<li class="single-post-wrapper__post">
+  wrapperEpisodes.innerHTML += `<li class="single-post-wrapper__post">
   <audio id="myAudio${ondemandEpisodData.id}">
     <source src="${ondemandEpisodData.broadcast.broadcastfiles[0].url}" type="audio/${audioType}">
     Your browser does not support the audio element.
@@ -165,11 +196,7 @@ wrapperEpisodes.innerHTML += `<li class="single-post-wrapper__post">
  </li>`; 
 }
 
-function setMainTitle(title, htmlId){
-document.getElementById(htmlId).innerHTML = title; 
-}
-
-/////////////////////AUDIO//////////////////////////
+/////////////////////AUDIO ON EPISODS-PAGE//////////////////////////
 
 function declareAudioTypeFromFileEnding(audioFile){
   let audioFileType = audioFile.substr(audioFile.length - 3);
@@ -191,7 +218,6 @@ function setListenButtons(){
 
 // When the button play or pause is clicked its playing or pausing the pod/broadcast
 function listenEpisode(event){
-  console.log("ich komme zu listenEpisode")
   let episodeId = parseInt(event.target.getAttribute("data-id"));
   
   if(auditivSignal != document.getElementById(`myAudio${episodeId}`)){
@@ -207,9 +233,10 @@ function listenEpisode(event){
 
 ///////////////////////////////TODO AND SUGGESTED IMPROVEMENTS//////////////////////////
 /*
-- clean bug: episodes with more than 168 or 243 are not loading more episodes
-  -> laoding only 100 and then have another page with other 100 in a next-button?
+- episodes with more than 168 or 243 are not loading more episodes so now there is a limit with 120
+  -> laoding only 120 now and then have another page with other 100 in a next-button etc.?
 - set icon for play and pause
+- set a progressbar for the time-played/still-to-play
 - when going back to main from episodes, make it back to the same program as before
   ->loading the page again with the same age-category-programs 
   ->scrolling down to the right program as before
